059a69536e28e26fbaa6caa1235048636274d8f7
sawonhossain34 <sawonhossain3491@gmail.com>
2023-02-15 09:28:16 +0600

    compleat shopping card

--- js/common.js
+++ js/common.js
@@ -4,13 +4,25 @@ function getTextElementValueById(elementId){
     const currentPhoneTotal = parseInt(currentPhoneTotalString);
     return currentPhoneTotal;
 }
+function setTextElementValueById(elementId,value){
+    const subTotalElement =document.getElementById(elementId);
+    subTotalElement.innerText = value;
+}
 
 function calculateSubTotal(){
     // calculate total
-    const currentPhoneTotal =document.getElementById('phone-total');
-    const currentCaseTotal = document.getElementById('case-total');
-    const currentSubTotal = currentPhoneTotal + currentCaseTotal;
-    const subTotalElement =document.getElementById('sub-total');
-    subTotalElement.innerText = currentSubTotal;
-    return currentSubTotal;
+    const currentPhoneTotal =getTextElementValueById('phone-total');
+    const currentCaseTotal = getTextElementValueById('case-total');
+
+    const currentSubTotal =currentPhoneTotal + currentCaseTotal;
+    setTextElementValueById('sub-total',currentSubTotal);
+
+    // calculate tax 
+    const taxtAmmountString = (currentSubTotal * 0.1).toFixed(2);
+    const taxtAmmount = parseInt(taxtAmmountString);
+    setTextElementValueById('tax-amount',taxtAmmount);
+
+    // calculate total
+    const finalAmmount = currentSubTotal + taxtAmmount;
+    setTextElementValueById('final-total',finalAmmount);
 }
